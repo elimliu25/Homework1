877a6798abea23b6a4396fa39e9d9c76870c2098
fizzbuzz <elim.liu@mail.utoronto.ca>
2025-05-15 17:50:30 -0400

    edited HW1

---
+++ src/Multiples.java
@@ -0,0 +1,22 @@
+public class Multiples {
+
+    // This is the modified main logic that returns the count.
+    public static int countMultiples(int n, int a, int b) {
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    // psvm for testing from the command line or IntelliJ
+    public static void main(String[] args) {
+        //how many numbers below 1000 are divisible by 3 or 5
+        int result = countMultiples(1000, 3, 5);
+        System.out.println("There are " + result + " multiples of 3 or 5 below 1000.");
+    }
+}
